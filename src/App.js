5968c294cee9ecfa3852d7e9a2d825e415aa9dc1
Kshitij <kshitij.mukh@gmail.com>
2024-12-05 23:35:54 +0100

    fix:   navigation fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import './App.css';
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirect /nastia to home */}
+        <Route path="/nastia" element={<Navigate to="/" />} />
+
+        {/* Other routes */}
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
